6e083439b1eb8073eaf47e943e1bc13262539377
Ni Ngo <ni.ngo@asnet.com.vn>
2021-01-12 14:42:25 +0700

    Add comment into access-ec2 shell file

--- ec2/access-ec2.sh
+++ ec2/access-ec2.sh
@@ -2,4 +2,5 @@
 chmod 400 ~/Downloads/FirstInstanceKeyPair.pem
 
 # Get Public IPv4 address of EC2 instance
+# Public IP will be changed when instance was stopped and restarted
 ssh -i ~/Downloads/FirstInstanceKeyPair.pem ec2-user@18.141.177.183
