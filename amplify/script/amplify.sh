581d9c78a7f6dee6f919eea156c043f40656ab3d
Ni Ngo <ni.ngo@asnet.com.vn>
2021-01-26 11:04:08 +0700

    Add hosting for app

--- amplify/script/amplify.sh
+++ amplify/script/amplify.sh
@@ -9,5 +9,12 @@ amplify update api
 # To build local BE. It will publish a valid endpoint
 amplify push
 
+# To add/update auth for api
+amplify add/update auth
+## Can create cognito user to test: test01@mail.com - Test@123
+
+# To add hosting, pick plugin module is Amplify console or S3/CloudFront
+amplify add hosting 
+
 # To build and publish whole app
 amplify publish
--- amplify/src/App.js
+++ amplify/src/App.js
@@ -1,8 +1,9 @@
 import Amplify from "aws-amplify"
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import Home from './page/Home'
 import awsExports from "./aws-exports"
 Amplify.configure(awsExports)
 
 const App = () => <Home />
 
-export default App
+export default withAuthenticator(App)
