a4c972d9dc8dcf96b2dd66adf5ca27ba36f232d9
Ni Ngo <ni.ngo@asnet.com.vn>
2021-01-28 14:02:52 +0700

    Connect graphql endpoint and allow to interact with api

--- amplify/amplify-egghead-poc/src/App.js
+++ amplify/amplify-egghead-poc/src/App.js
@@ -6,16 +6,24 @@ import Home from './page/Home'
 import './App.css';
 
 // Configure amplify with generated values
+import { MyTheme, SignupFormConfig } from './config/authenticator-config'
 import Amplify from 'aws-amplify'
 import awsExports from "./aws-exports"
-import { MyTheme, SignupFormConfig } from './config/authenticator-config'
 Amplify.configure(awsExports)
 
 const App = () => <Home />
 
 export default withAuthenticator(App, 
   true, // Include user name and signout button
-  [], 
+  [
+    // <AuthGreeting override='Greetings'/>,
+    // <ConfirmSignIn/>,
+    // <VerifyContact/>,
+    // <SignUp/>,
+    // <ConfirmSignUp/>,
+    // <ForgotPassword/>,
+    // <RequireNewPassword />
+  ], 
   null, // Not include federate Google, Facebook
   MyTheme, 
   // Signup configuration
--- amplify/amplify-egghead-poc/src/page/styles.js
+++ amplify/amplify-egghead-poc/src/page/styles.js
@@ -1,8 +1,8 @@
 export const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
+  book: {  marginBottom: 15 },
   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
+  bookName: { fontSize: 20, fontWeight: 'bold' },
+  bookDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
