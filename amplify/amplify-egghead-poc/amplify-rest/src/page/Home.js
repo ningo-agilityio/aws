e0436862fbf1880c54c850017429b301b5f3c49f
Ni Ngo <ni.ngo@asnet.com.vn>
2021-01-28 16:21:32 +0700

    Init amplify rest poc

--- amplify/amplify-egghead-poc/amplify-rest/src/page/Home.js
+++ amplify/amplify-egghead-poc/amplify-rest/src/page/Home.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { styles } from './styles'
-import { API, graphqlOperation } from 'aws-amplify'
-import { createBook } from '../graphql/mutations'
-import { listBooks } from '../graphql/queries'
+import { API } from 'aws-amplify'
 
 const initialState = { name: '', description: '' }
 
@@ -22,8 +20,8 @@ const Home = () => {
 
   const fetchData = async () => {
     try {
-      const response = await API.graphql(graphqlOperation(listBooks))
-      setData(response.data.listBooks.items)
+      // const response = await API.graphql(graphqlOperation(listBooks))
+      setData([])
     } catch(err) {
       setError("Something wrong during fetching data")
     } finally {
@@ -31,15 +29,15 @@ const Home = () => {
     }
   }
 
-  const addBook = async () => {
+  const addEmployee = async () => {
     try {
       if (!formState.name || !formState.description) {
         return
       }
-      const book = { ...formState }
-      setData([...data, book])
+      const employee = { ...formState }
+      setData([...data, employee])
       setFormState(initialState)
-      await API.graphql(graphqlOperation(createBook, {input: book}))
+      // await API.graphql(graphqlOperation(createBook, {input: book}))
     } catch (err) {
       setError("Something wrong during creating new book")
     } finally {
@@ -49,7 +47,7 @@ const Home = () => {
 
   return (
     <div style={styles.container}>
-      <h2>Books Managements App</h2>
+      <h2>Employees Managements App</h2>
       {error && <label>{error}</label>}
       <input
         onChange={event => setInput('name', event.target.value)}
@@ -57,13 +55,7 @@ const Home = () => {
         value={formState.name}
         placeholder="Name"
       />
-      <input
-        onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addBook}>Add new book</button>
+      <button style={styles.button} onClick={addEmployee}>Add new employee</button>
       {
         processing && <p>Loading...</p>
       }
